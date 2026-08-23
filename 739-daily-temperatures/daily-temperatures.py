bd7932340b60043cdc91c581acdecd071df69cc2
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2026-08-23 11:48:53 +0530

    Time: 111 ms (31.00%) | Memory: 28.1 MB (79.07%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+        st=[]
+        ans=[0]*len(temp)
+        for i in range(len(temp)-1,-1,-1):
+            while st and temp[st[-1]] <= temp[i]:
+                st.pop()
+            if st :
+                ans[i]=st[-1]-i
+            st.append(i)
+        return ans
